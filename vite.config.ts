90e663db942894b52b432c0265783ef7f5995dbc
K0rzet <ehot2004@gmail.com>
2024-10-12 23:51:14 +0300

    fixed errors for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/styles/variables.scss";`,
-      },
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
